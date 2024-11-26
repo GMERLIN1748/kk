a43c04be9b13192a28409267b9075e25f475eeca
GMERLIN1748 <krittin.i@kkumail.com>
2024-11-26 11:24:57 +0700

    add

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+string name = "krittin Intrakkol";
+string id = "673450030-6";
+// string is Datatype -> ชนิดข้อมูล
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is krittin Intrakool");
+Console.WriteLine("My ID :673450030-6");
